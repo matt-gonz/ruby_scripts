1f076bc247a18bb7e64e81df0f62fac84be6e251
Matthew <matthewgonzalez2324@gmail.com>
2022-07-18 22:34:08 -0700

    Allow function to sort until array is in asc order

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,16 +1,24 @@
+def sorter(my_array)
+  n = my_array.length - 1
+  n.times{bubble_sort(my_array)}
+  my_array
+end
+
 def bubble_sort(my_array)
-  is_sorted = false
   my_array.each_with_index do |value, index|
+    is_sorted = false
     current_number = value
     if my_array[index+1] != nil
       next_number = my_array[index+1]
       if current_number > next_number
         my_array[index], my_array[index+1] = my_array[index+1], my_array[index]
       end
+    else
+      is_sorted = true
     end
   end
-  puts my_array
+  my_array
 end
 
 my_array = [4,3,78,2,0,2]
-bubble_sort(my_array)
+puts sorter(my_array)
